90fedcfcff96ef8d7a04892ac34089ab0e31b95d
2hyerin <88357058+2hyerin@users.noreply.github.com>
2021-10-19 21:57:51 +0900

    Add files via upload
    2021.10.19 이혜린 - 일대일문의 챗봇

---
+++ [일대일문의] 관리자/myChat/chatServer.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace myChat
+{
+    partial class chatServer
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(chatServer));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbClientList = new System.Windows.Forms.ToolStripDropDownButton();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.tbClient = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pmnuServerStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbClientList});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(537, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbClientList
+            // 
+            this.sbClientList.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.sbClientList.Image = ((System.Drawing.Image)(resources.GetObject("sbClientList.Image")));
+            this.sbClientList.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.sbClientList.Name = "sbClientList";
+            this.sbClientList.Size = new System.Drawing.Size(90, 24);
+            this.sbClientList.Text = "Client List";
+            this.sbClientList.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.sbClientList_DropDownItemClicked);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tbServer);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tbClient);
+            this.splitContainer2.Size = new System.Drawing.Size(534, 419);
+            this.splitContainer2.SplitterDistance = 199;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // tbServer
+            // 
+            this.tbServer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbServer.Location = new System.Drawing.Point(3, 3);
+            this.tbServer.Multiline = true;
+            this.tbServer.Name = "tbServer";
+            this.tbServer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbServer.Size = new System.Drawing.Size(528, 193);
+            this.tbServer.TabIndex = 0;
+            // 
+            // tbClient
+            // 
+            this.tbClient.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClient.ContextMenuStrip = this.contextMenuStrip1;
+            this.tbClient.Location = new System.Drawing.Point(3, 3);
+            this.tbClient.Multiline = true;
+            this.tbClient.Name = "tbClient";
+            this.tbClient.Size = new System.Drawing.Size(528, 210);
+            this.tbClient.TabIndex = 1;
+            this.tbClient.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbClient_KeyDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pmnuServerStart});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(151, 28);
+            // 
+            // pmnuServerStart
+            // 
+            this.pmnuServerStart.Name = "pmnuServerStart";
+            this.pmnuServerStart.Size = new System.Drawing.Size(150, 24);
+            this.pmnuServerStart.Text = "ServerStart";
+            this.pmnuServerStart.Click += new System.EventHandler(this.pmnuServerStart_Click);
+            // 
+            // chatServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 446);
+            this.Controls.Add(this.splitContainer2);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "chatServer";
+            this.Text = "[1 : 1 문의] 관리자";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmChat_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton sbClientList;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.TextBox tbClient;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pmnuServerStart;
+    }
+}
+
---
+++ [일대일문의] 관리자/myChat/chatServer.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myChat
+{
+    public partial class chatServer : Form
+    {
+        public chatServer()
+        {
+            InitializeComponent();
+        }
+
+        delegate void cbAddText(string str, int i);
+        void AddText(string str, int i)
+        {
+            if (tbServer.InvokeRequired || tbClient.InvokeRequired ||statusStrip1.InvokeRequired)
+            {
+                cbAddText cb = new cbAddText(AddText);
+                object[] obj = { str, i };
+                Invoke(cb, obj);
+            }
+            else
+            {
+                if (i == 1)
+                    tbServer.Text += str;
+                else if (i == 2)
+                    tbClient.Text += str;
+                else if (i == 3)
+                    sbClientList.DropDownItems.Add(str);
+            }
+        }
+        class TcpEx
+        {
+            public TcpClient tp;
+            public string id;
+            public TcpEx(TcpClient t, string s)
+            {
+                tp = t;
+                id = s;
+            }
+        }
+
+        
+        Socket sock = null;
+        TcpClient[] tcp = new TcpClient[100];
+        TcpListener listen = null;
+        Thread threadServer = null; // Connect 요구 처리 쓰레드
+        Thread threadRead = null;   // 입력 문자열 처리 쓰레드
+        int CurrentClientNum = 0;
+        int ServerPort = 9000;
+
+        string[] sname = new string[100];
+        void ServerProcess()// Connect 요구 처리 쓰레드
+        {
+            while (true)
+            {
+                if (listen.Pending())
+                {
+                    if (CurrentClientNum == 99) break;
+                    tcp[CurrentClientNum] = listen.AcceptTcpClient(); // 세션 수립                    
+                    string sLabel = tcp[CurrentClientNum].Client.RemoteEndPoint.ToString();
+                    AddText($"Client [{sLabel}] 로부터 접속되었습니다\r\n\r\n", 1);
+                    AddText(sLabel, 3);
+                    sbClientList.Text = sLabel;
+                    
+                    sname[CurrentClientNum] = tcp[CurrentClientNum].Client.RemoteEndPoint.ToString();
+
+                    if (threadRead == null)
+                    {
+                        threadRead = new Thread(ReadProcess);
+                        threadRead.Start();
+                    }
+                    CurrentClientNum++;
+                }
+                Thread.Sleep(100);
+            }
+        }        
+
+        void ReadProcess()
+        {
+            
+            byte[] bArr = new byte[512];
+            while (true)
+            {
+                for(int i = 0; i < CurrentClientNum; i++)
+                {
+                    NetworkStream ns = tcp[i].GetStream();
+                    string snn = sname[i];
+                    if (ns.DataAvailable)
+                    {                        
+                        int n = ns.Read(bArr, 0, 512);
+                        AddText($"[{snn}] : {Encoding.Default.GetString(bArr, 0, n)}", 1);
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void frmChat_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (threadServer != null) threadServer.Abort();
+            if (threadRead != null) threadRead.Abort();
+        }
+
+        void ClientReadProcess()
+        {
+            byte[] bArr = new byte[512];
+            while (true)
+            {
+                if (sock.Available > 0)
+                {
+                    int n = sock.Receive(bArr);
+                    AddText($"{Encoding.Default.GetString(bArr, 0, n)}", 2);
+                }
+                Thread.Sleep(100);
+            }
+        }
+             
+
+        private void tbClient_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                int j = 0;
+                for(int i = 0; i < CurrentClientNum; i++)
+                {
+                    if (tcp[i].Client.RemoteEndPoint.ToString() == sbClientList.Text)
+                        j = i;
+                }
+                tcp[j].Client.Send(Encoding.Default.GetBytes(tbClient.Text+"\r\n"));
+                string sn = sname[j];
+                AddText($"[{sn} 답변] : {tbClient.Text}", 1);
+                tbClient.Text = "";
+            }
+        }
+
+        private void pmnuServerStart_Click(object sender, EventArgs e)
+        {
+            if (listen != null)
+            {
+                if (MessageBox.Show("서버를 다시 시작하시겠습니까?.", "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    listen.Stop();
+                    if (threadServer != null) threadServer.Abort();
+                    if (threadRead != null) threadRead.Abort();
+                }
+            }
+            listen = new TcpListener(ServerPort);
+            listen.Start();
+            AddText($"서버가 [{ServerPort}] Port에서 시작되었습니다.\r\n\r\n", 1);
+
+            threadServer = new Thread(ServerProcess);
+            threadServer.Start();
+        }
+
+        private void sbClientList_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            sbClientList.Text = e.ClickedItem.Text;
+        }
+    }
+}
